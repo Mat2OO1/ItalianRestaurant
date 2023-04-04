665038fd5638a336e3cb373b5fac29c2d9fb7f4f
Mat2OO1 <mateuszt.krajewski@gmail.com>
2023-04-04 12:23:58 +0200

    Created project structure, welcome view and login and register view

---
+++ src/main/java/com/example/italianrestaurant/ItalianRestaurantApplication.java
@@ -0,0 +1,13 @@
+package com.example.italianrestaurant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItalianRestaurantApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItalianRestaurantApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/italianrestaurant/ItalianRestaurantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.italianrestaurant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItalianRestaurantApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
