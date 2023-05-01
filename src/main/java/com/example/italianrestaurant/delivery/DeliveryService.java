fd3be0de2e13f683389cb7cc84d664bdb655fb3b
PawelK <pawel.kluska256@gmail.com>
2023-05-01 11:27:58 +0200

    add delivery unit tests

--- src/main/java/com/example/italianrestaurant/delivery/DeliveryService.java
+++ src/main/java/com/example/italianrestaurant/delivery/DeliveryService.java
@@ -1,5 +1,6 @@
 package com.example.italianrestaurant.delivery;
 
+import com.example.italianrestaurant.exceptions.InvalidEntityException;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
@@ -13,8 +14,16 @@ public class DeliveryService {
     private final DeliveryRepository deliveryRepository;
     private final ModelMapper modelMapper;
 
-    public Delivery addDelivery(DeliveryDto deliveryDto) {
+    public Delivery addDelivery(DeliveryDto deliveryDto) throws InvalidEntityException {
         Delivery delivery = modelMapper.map(deliveryDto, Delivery.class);
+        if (!isDeliveryValid(delivery)) throw new InvalidEntityException();
         return deliveryRepository.save(delivery);
+    }
+
+    public boolean isDeliveryValid(Delivery delivery) {
+        return delivery.getAddress() != null &&
+                delivery.getCity() != null &&
+                delivery.getPostalCode() != null &&
+                delivery.getFloor() != null;
     }
 }
---
+++ src/test/java/com/example/italianrestaurant/delivery/DeliveryServiceTest.java
@@ -0,0 +1,68 @@
+package com.example.italianrestaurant.delivery;
+
+import com.example.italianrestaurant.Utils;
+import com.example.italianrestaurant.exceptions.InvalidEntityException;
+import lombok.val;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.modelmapper.ModelMapper;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+
+@ExtendWith(MockitoExtension.class)
+public class DeliveryServiceTest {
+
+    @Mock
+    private DeliveryRepository deliveryRepository;
+    @Mock
+    private ModelMapper modelMapper;
+    @InjectMocks
+    private DeliveryService deliveryService;
+
+
+    @Test
+    void shouldAddDelivery() throws InvalidEntityException {
+
+        // given
+        val deliveryDto = Utils.getDeliveryDto();
+        val mappedDelivery = Utils.getDelivery();
+        val dbDelivery = Utils.getDelivery();
+        dbDelivery.setId(1L);
+
+        given(modelMapper.map(deliveryDto, Delivery.class)).willReturn(mappedDelivery);
+        given(deliveryRepository.save(mappedDelivery)).willReturn(dbDelivery);
+
+        // when
+        val returnedDelivery = deliveryService.addDelivery(deliveryDto);
+
+        // then
+        verify(deliveryRepository).save(mappedDelivery);
+        assertThat(returnedDelivery).isEqualTo(dbDelivery);
+    }
+
+    @Test
+    void shouldNotAddDelivery() throws InvalidEntityException {
+
+        // given
+        val deliveryDto = new DeliveryDto();
+        val mappedDelivery = new Delivery();
+
+        given(modelMapper.map(deliveryDto, Delivery.class)).willReturn(mappedDelivery);
+
+        // when
+        assertThatThrownBy(() -> deliveryService.addDelivery(deliveryDto)).
+                isInstanceOf(InvalidEntityException.class);
+        // then
+        verify(deliveryRepository, times(0)).save(any());
+    }
+
+}
