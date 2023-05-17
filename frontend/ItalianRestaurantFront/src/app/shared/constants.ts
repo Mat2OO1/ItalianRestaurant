57d2d56b3b9ca59a23d6b2868e30e7c7a2bd3313
PawelK <pawel.kluska256@gmail.com>
2023-05-17 18:36:11 +0200

    fix url in fron oauth

--- frontend/ItalianRestaurantFront/src/app/shared/constants.ts
+++ frontend/ItalianRestaurantFront/src/app/shared/constants.ts
@@ -1,7 +1,9 @@
+import {environment} from "../../environments/environment";
+
 export class AppConstants {
-  private static API_BASE_URL = "http://localhost:8080/";
+  private static API_BASE_URL = environment.apiUrl + "/";
   private static OAUTH2_URL = AppConstants.API_BASE_URL + "oauth2/authorize/";
-  private static REDIRECT_URL = "?redirect_uri=http://localhost:4200/oauth2/redirect"
+  private static REDIRECT_URL = `?redirect_uri=${environment.frontUrl}/oauth2/redirect`
   public static GOOGLE_AUTH_URL = AppConstants.OAUTH2_URL + "google" + AppConstants.REDIRECT_URL;
   public static GITHUB_AUTH_URL = AppConstants.OAUTH2_URL + "github" + AppConstants.REDIRECT_URL;
 }
