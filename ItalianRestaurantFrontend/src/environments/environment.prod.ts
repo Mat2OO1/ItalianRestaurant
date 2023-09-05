228bed01530c5f07e59e779fc17f274a14df05e4
PawelK <pawel.kluska256@gmail.com>
2023-09-05 23:00:31 +0200

    adapt addresses

--- ItalianRestaurantFrontend/src/environments/environment.prod.ts
+++ ItalianRestaurantFrontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: "http://localhost:8080",
-  frontUrl: "http://localhost"
+  apiUrl: "http://13.50.29.145:8080",
+  frontUrl: "http://13.50.29.145"
 };
