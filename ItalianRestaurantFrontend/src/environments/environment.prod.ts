98ecfeacd80c6131d53c74e9dc5ed5b8d1e071dc
PawelK <pawel.kluska256@gmail.com>
2023-09-20 21:50:28 +0200

    change front prod url

--- ItalianRestaurantFrontend/src/environments/environment.prod.ts
+++ ItalianRestaurantFrontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: "http://13.50.29.145:8080",
-  frontUrl: "http://13.50.29.145"
+  apiUrl: "http://ladolcevitarestaurant.live:8080",
+  frontUrl: "http://ladolcevitarestaurant.live"
 };
