7740f0c0d378ccf3343f7a71be4ed42b4dde6f9b
Sebastian <sebo21cc21@gmail.com>
2023-11-27 12:23:26 +0100

    Add snackbar service on admin panel

--- ItalianRestaurantFrontend/src/app/admin-panel/admin-panel.component.ts
+++ ItalianRestaurantFrontend/src/app/admin-panel/admin-panel.component.ts
@@ -3,6 +3,8 @@ import {DataStorageService} from "../shared/data-storage.service";
 import {OrderRes} from "../models/order";
 import {FormBuilder, FormControl, FormGroup} from "@angular/forms";
 import {formatDate} from "@angular/common";
+import {SnackbarService} from "../shared/sncakbar.service";
+import {TranslateService} from "@ngx-translate/core";
 
 @Component({
   selector: 'app-admin-panel',
@@ -17,7 +19,9 @@ export class AdminPanelComponent implements OnDestroy{
   lang = ""
 
   constructor(private dataStorageService: DataStorageService,
-              private formBuilder: FormBuilder) {
+              private formBuilder: FormBuilder,
+              private snackbarService: SnackbarService,
+              private translate: TranslateService) {
     this.dataStorageService.getOrders()
       .subscribe(
         (res) => {
@@ -51,6 +55,9 @@ export class AdminPanelComponent implements OnDestroy{
     deliveryDate.setMinutes(form.value['deliveryDate'].substring(3, 5))
     let deliveryDateFormatted = formatDate(deliveryDate, 'YYYY-MM-ddTHH:mm:ss', 'en-GB')
     this.dataStorageService.updateOrder(orderStatus, deliveryDateFormatted, orderId)
+    this.translate.get('admin_panel_change_snack').subscribe((message) => {
+      this.snackbarService.openSnackbarSuccess(message);
+    });
   }
   getDeliveryOptionsTranslation(deliveryOptions: string): string {
     this.lang = localStorage.getItem('lang') || 'en';
@@ -98,7 +105,11 @@ export class AdminPanelComponent implements OnDestroy{
         if (index !== -1) {
           this.orders.splice(index, 1);
         }
+        this.translate.get('admin_panel_delete_snack').subscribe((message) => {
+          this.snackbarService.openSnackbarError(message);
+        });
       }
     );
+
   }
 }
